68a3f0f164edcd0609ac1f817806b423a4af6e82
Jiehao Guan <jguan66@gatech.edu>
2026-02-04 00:03:52 -0800

    Started with basic methods

--- gitlet/Main.java
+++ gitlet/Main.java
@@ -9,16 +9,37 @@ public class Main {
      *  <COMMAND> <OPERAND1> <OPERAND2> ... 
      */
     public static void main(String[] args) {
-        // TODO: what if args is empty?
+        if (args.length == 0) {
+            System.out.println("Please enter a command.");
+            return;
+        }
+
         String firstArg = args[0];
-        switch(firstArg) {
-            case "init":
-                // TODO: handle the `init` command
-                break;
-            case "add":
-                // TODO: handle the `add [filename]` command
-                break;
-            // TODO: FILL THE REST IN
+        try {
+            switch (firstArg) {
+                case "init":
+                    Repository.init();
+                    break;
+                case "add":
+                    checkArgs(args, 2);
+                    Repository.add(args[1]);
+                    break;
+                case "commit":
+                    checkArgs(args, 2);
+                    Repository.commit(args[1]);
+                    break;
+                // Add more cases as you go (log, checkout, branch)
+                default:
+                    System.out.println("No command with that name exists.");
+            }
+        } catch (GitletException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void checkArgs(String[] args, int expectedCount) {
+        if (args.length != expectedCount) {
+            throw new GitletException("Incorrect operands.");
         }
     }
 }
--- gitlet/Repository.java
+++ gitlet/Repository.java
@@ -2,6 +2,7 @@ package gitlet;
 
 import java.io.File;
 import static gitlet.Utils.*;
+import java.util.TreeMap;
 
 // TODO: any imports you need here
 
@@ -24,6 +25,38 @@ public class Repository {
     public static final File CWD = new File(System.getProperty("user.dir"));
     /** The .gitlet directory. */
     public static final File GITLET_DIR = join(CWD, ".gitlet");
+    /** Directory for all objects (Commits/Blobs) */
+    public static final File OBJECTS_DIR = join(GITLET_DIR, "objects");
+    /** Directory for branch heads */
+    public static final File HEADS_DIR = join(GITLET_DIR, "refs", "heads");
 
     /* TODO: fill in the rest of this class. */
+    public static void init() {
+        if (GITLET_DIR.exists()) {
+            System.out.println("A Gitlet version-control system already exists in the current directory.");
+            return;
+        }
+
+        // Create directory structure
+        GITLET_DIR.mkdir();
+        OBJECTS_DIR.mkdir();
+        join(GITLET_DIR, "refs").mkdir();
+        HEADS_DIR.mkdir();
+
+        // 1. Create the Genesis Commit (No files, Unix Epoch time)
+        Commit initialCommit = new Commit("initial commit", null, new TreeMap<>());
+        
+        // 2. Persist the commit using Utils.writeObject
+        String commitHash = sha1(serialize(initialCommit));
+        File commitFile = join(OBJECTS_DIR, commitHash);
+        writeObject(commitFile, initialCommit);
+
+        // 3. Create 'master' branch head
+        File masterBranch = join(HEADS_DIR, "master");
+        writeContents(masterBranch, commitHash);
+
+        // 4. Create HEAD pointing to master branch
+        File head = join(GITLET_DIR, "HEAD");
+        writeContents(head, "ref: refs/heads/master");
+    }
 }
