3edd5b3d16e011f9ba8118eef971cf4850b6eb39
Jiehao Guan <jguan66@gatech.edu>
2026-02-04 22:41:23 -0800

    Updated commit class to handle second parent

--- gitlet/Commit.java
+++ gitlet/Commit.java
@@ -8,19 +8,10 @@ import java.util.Map;
 import java.util.TreeMap;
 
 /** Represents a gitlet commit object.
- *  TODO: It's a good idea to give a description here of what else this Class
- *  does at a high level.
  *
- *  @author TODO
+ *  @author Jiehao Guan
  */
 public class Commit implements Serializable {
-    /**
-     * TODO: add instance variables here.
-     *
-     * List all instance variables of the Commit class here with a useful
-     * comment above them describing what that variable represents and how that
-     * variable is used. We've provided one example for `message`.
-     */
 
     /** The message of this Commit. */
     private String message;
